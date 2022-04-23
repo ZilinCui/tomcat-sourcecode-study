60170bead499489eb5a941c0276bfbb29ce28d67
localczl <2508632323@qq.com>
2022-04-23 18:57:22 +0800

    app2 finished

---
+++ simpleservletcontainer/src/main/java/top/cuizilin/study/app2/Constants.java
@@ -0,0 +1,8 @@
+package top.cuizilin.study.app2;
+
+import java.io.File;
+
+public class Constants {
+    public static final String WEB_ROOT =
+            System.getProperty("user.dir") + File.separator + "simpleservletcontainer" + File.separator  + "webroot";
+}
---
+++ simpleservletcontainer/src/main/java/top/cuizilin/study/app2/RequestFacade.java
@@ -0,0 +1,200 @@
+package top.cuizilin.study.app2;
+
+import javax.servlet.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Map;
+
+public class RequestFacade implements ServletRequest {
+    private ServletRequest request;
+
+    public RequestFacade(Request request) {
+        this.request = request;
+    }
+
+    public void setRequest(ServletRequest servletRequest){
+       this.request = servletRequest;
+    }
+    @Override
+    public Object getAttribute(String s) {
+        return request.getAttribute(s);
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+        return request.getAttributeNames();
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return request.getCharacterEncoding();
+    }
+
+    @Override
+    public void setCharacterEncoding(String s) throws UnsupportedEncodingException {
+        request.setCharacterEncoding(s);
+    }
+
+    @Override
+    public int getContentLength() {
+        return request.getContentLength();
+    }
+
+    @Override
+    public String getContentType() {
+        return request.getContentType();
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        return request.getInputStream();
+    }
+
+    @Override
+    public String getParameter(String s) {
+        return request.getParameter(s);
+    }
+
+    @Override
+    public Enumeration<String> getParameterNames() {
+        return request.getParameterNames();
+    }
+
+    @Override
+    public String[] getParameterValues(String s) {
+        return request.getParameterValues(s);
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+        return request.getParameterMap();
+    }
+
+    @Override
+    public String getProtocol() {
+        return request.getProtocol();
+    }
+
+    @Override
+    public String getScheme() {
+        return request.getScheme();
+    }
+
+    @Override
+    public String getServerName() {
+        return getServerName();
+    }
+
+    @Override
+    public int getServerPort() {
+        return request.getServerPort();
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return request.getReader();
+    }
+
+    @Override
+    public String getRemoteAddr() {
+        return request.getRemoteAddr();
+    }
+
+    @Override
+    public String getRemoteHost() {
+        return request.getRemoteHost();
+    }
+
+    @Override
+    public void setAttribute(String s, Object o) {
+        request.setAttribute(s, o);
+    }
+
+    @Override
+    public void removeAttribute(String s) {
+        request.removeAttribute(s);
+    }
+
+    @Override
+    public Locale getLocale() {
+        return request.getLocale();
+    }
+
+    @Override
+    public Enumeration<Locale> getLocales() {
+        return request.getLocales();
+    }
+
+    @Override
+    public boolean isSecure() {
+        return request.isSecure();
+    }
+
+    @Override
+    public RequestDispatcher getRequestDispatcher(String s) {
+        return request.getRequestDispatcher(s);
+    }
+
+    @Override
+    public String getRealPath(String s) {
+        return request.getRealPath(s);
+    }
+
+    @Override
+    public int getRemotePort() {
+        return request.getRemotePort();
+    }
+
+    @Override
+    public String getLocalName() {
+        return request.getLocalName();
+    }
+
+    @Override
+    public String getLocalAddr() {
+        return request.getLocalAddr();
+    }
+
+    @Override
+    public int getLocalPort() {
+        return request.getLocalPort();
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return request.getServletContext();
+    }
+
+    @Override
+    public AsyncContext startAsync() {
+        return request.startAsync();
+    }
+
+    @Override
+    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) {
+        return request.startAsync(servletRequest, servletResponse);
+    }
+
+    @Override
+    public boolean isAsyncStarted() {
+        return request.isAsyncSupported();
+    }
+
+    @Override
+    public boolean isAsyncSupported() {
+        return request.isAsyncSupported();
+    }
+
+    @Override
+    public AsyncContext getAsyncContext() {
+        return request.getAsyncContext();
+    }
+
+    @Override
+    public DispatcherType getDispatcherType() {
+        return request.getDispatcherType();
+    }
+}
