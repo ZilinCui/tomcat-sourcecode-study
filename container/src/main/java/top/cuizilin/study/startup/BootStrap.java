f3fee897df189d0ad0d62bb728f97096a56b1286
localczl <2508632323@qq.com>
2022-05-01 01:21:48 +0800

    finish the standard class loader

--- container/src/main/java/top/cuizilin/study/startup/BootStrap.java
+++ container/src/main/java/top/cuizilin/study/startup/BootStrap.java
@@ -2,66 +2,63 @@ package top.cuizilin.study.startup;
 
 
 import org.apache.catalina.*;
-import top.cuizilin.study.connector.http.HttpConnector;
+import org.apache.catalina.connector.http.HttpConnector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.loader.WebappClassLoader;
+import org.apache.catalina.loader.WebappLoader;
+import org.apache.naming.resources.ProxyDirContext;
 import top.cuizilin.study.core.*;
 import top.cuizilin.study.loader.SimpleWebappLoader;
 import top.cuizilin.study.logger.FileLogger;
 
 public final class BootStrap {
     public static void main(String[] args) {
+
+        System.setProperty("catalina.base", System.getProperty("user.dir"));
+        Connector connector = new HttpConnector();
         Wrapper wrapper1 = new SimpleWrapper();
-        wrapper1.setName("Primitive");
-        wrapper1.setServletClass("PrimitiveServlet");
+        wrapper1.setName("AServlet");
+        wrapper1.setServletClass("top.cuizilin.servlet.AServlet");
 
-        Wrapper wrapper2 = new SimpleWrapper();
-        wrapper2.setName("Modern");
-        wrapper2.setServletClass("ModernServlet");
 
-        Wrapper wrapper3 = new SimpleWrapper();
-        wrapper3.setName("AServlet");
-        wrapper3.setServletClass("top.cuizilin.webdemo.AServlet");
+        Context context = new StandardContext();
+        // StandardContext's start method adds a default mapper
+        context.setPath("webapp/web");
+        context.setDocBase("webapp/web");
 
-        Context context = new SimpleContext();
         context.addChild(wrapper1);
-        context.addChild(wrapper2);
-        context.addChild(wrapper3);
 
-        Mapper mapper = new SimpleContextMapper();
-        mapper.setProtocol("http");
-        context.addMapper(mapper);
-
-        LifecycleListener listener = new SimpleContextLifecycleListener();
+        context.addServletMapping("/AServlet", "AServlet");
+        // add ContextConfig. This listener is important because it configures
+        // StandardContext (sets configured to true), otherwise StandardContext
+        // won't start
+        LifecycleListener listener = new SimpleContextConfig();
         ((Lifecycle) context).addLifecycleListener(listener);
 
-        SimpleWebappLoader loader = new SimpleWebappLoader();
+        // here is our loader
+        Loader loader = new WebappLoader();
+        // associate the loader with the Context
         context.setLoader(loader);
 
-
-        // context.addServletMapping(pattern, name);
-        context.addServletMapping("/Primitive", "Primitive");
-        context.addServletMapping("/Modern", "Modern");
-        context.addServletMapping("/AServlet", "AServlet");
-
-        // ------ add logger --------
-        System.setProperty("catalina.base", System.getProperty("user.dir"));
-        FileLogger logger = new FileLogger();
-        logger.setPrefix("FileLog_");
-        logger.setSuffix(".txt");
-        logger.setTimestamp(true);
-        logger.setDirectory("D:\\SOFT\\program\\sourceCodeStudy\\study\\container\\webroot");
-        context.setLogger(logger);
-
-        HttpConnector connector = new HttpConnector();
-
         connector.setContainer(context);
+
         try {
-            connector.start();
+            connector.initialize();
+            ((Lifecycle) connector).start();
             ((Lifecycle) context).start();
+            // now we want to know some details about WebappLoader
+            WebappClassLoader classLoader = (WebappClassLoader) loader.getClassLoader();
+            System.out.println("Resources' docBase: " + ((ProxyDirContext)classLoader.getResources()).getDocBase());
+            String[] repositories = classLoader.findRepositories();
+            for (int i=0; i<repositories.length; i++) {
+                System.out.println("  repository: " + repositories[i]);
+            }
 
             // make the application wait until we press a key.
             System.in.read();
             ((Lifecycle) context).stop();
-        }catch (Exception e) {
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
